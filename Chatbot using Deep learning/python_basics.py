dcac28c40c935820241c51d12203b02a76153548
Naveen <naveen.chaurasia861@gmail.com>
2023-03-13 18:55:34 +0530

    added dict basics

---
+++ Chatbot using Deep learning/python_basics.py
@@ -0,0 +1,17 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.items():
+  print(x[0])
+  print(x[1])
+
+
+  for x in thisdict.keys():
+    print (x)
+
+
+  for x in thisdict.values():
+    print(x)   
+
